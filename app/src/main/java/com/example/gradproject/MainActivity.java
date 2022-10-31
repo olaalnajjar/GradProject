1b0a2ed585a5169f960e9dd456d3f77486300877
olaalnajjar <ola.alnajjar@hotmail.com>
2022-10-31 20:27:09 +0200

    Map Page and Navigation bar implemented

--- app/src/main/java/com/example/gradproject/MainActivity.java
+++ app/src/main/java/com/example/gradproject/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.gradproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.gradproject.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MapFragment());
+        binding.navView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.NavMap:
+                    replaceFragment(new MapFragment());
+                    break;
+                case R.id.NavCamera:
+                    replaceFragment(new CameraFragment());
+                    break;
+                case R.id.NavAnalysis:
+                    replaceFragment(new AnalysisFragment());
+                    break;
+            }
+            return true;
+        });
+
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_frag,fragment);
+        fragmentTransaction.commit();
     }
 }
