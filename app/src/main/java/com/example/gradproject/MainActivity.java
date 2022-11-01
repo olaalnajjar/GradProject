665c6c44d176352ff57efd4e048da970ea0f6707
olaalnajjar <ola.alnajjar@hotmail.com>
2022-11-01 19:57:18 +0200

    Settings Drawer added

--- app/src/main/java/com/example/gradproject/MainActivity.java
+++ app/src/main/java/com/example/gradproject/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gradproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,13 +15,22 @@ import com.example.gradproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
     ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding= ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer =(DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
         replaceFragment(new MapFragment());
         binding.navView.setOnItemSelectedListener(item -> {
 
@@ -38,6 +51,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     private void replaceFragment(Fragment fragment){
