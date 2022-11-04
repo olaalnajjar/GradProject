64a3fce91308d8f6e4aeb000f0c01bf102fb8f2d
olaalnajjar <ola.alnajjar@hotmail.com>
2022-11-04 18:44:01 +0200

    Toolbar colors changed

--- app/src/main/java/com/example/gradproject/MainActivity.java
+++ app/src/main/java/com/example/gradproject/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         drawer =(DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.black));
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
