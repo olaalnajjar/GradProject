be46356941c921ee2698312392f9fc7c8f290795
Mansi74 <yousefmansi74@gmail.com>
2022-12-08 17:59:16 +0200

    splash screen

---
+++ app/src/main/java/com/example/gradproject/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.gradproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1250);
+    }
+}
