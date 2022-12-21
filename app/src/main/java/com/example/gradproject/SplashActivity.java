ca376369c39e3047f4179dc578abed7655442f60
olaalnajjar <ola.alnajjar@hotmail.com>
2022-12-21 20:56:51 +0200

    Making splash screen appear in full screen mode

--- app/src/main/java/com/example/gradproject/SplashActivity.java
+++ app/src/main/java/com/example/gradproject/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //configuring window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
